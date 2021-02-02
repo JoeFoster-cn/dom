c01062c502b8692ef3f4d0e455ed3cd40a419ca3
Joe Foster <jfoster29492@gmail.com>
2021-02-02 16:08:44 +0000

    added html and js files

---
+++ 5-activities/app.js
@@ -0,0 +1,4 @@
+const placeholder = document.getElementById("placeholder");
+const input = document.getElementById("input");
+const submit = document.getElementById("submit");
+const list = document.getElementById("list");
