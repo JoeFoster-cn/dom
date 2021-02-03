08e212b41f1fb62567314491882f764c9d6fea9d
Joe Foster <jfoster29492@gmail.com>
2021-02-03 13:10:05 +0000

    added keydown event listeners
    keydown key information successfully showing

--- keycode/app.js
+++ keycode/app.js
@@ -6,6 +6,8 @@ const eventKey = document.querySelector("#eventkey");
 const eventCode = document.querySelector("#eventcode");
 const eventWhich = document.querySelector("#eventwhich");
 
+//const location = KeyboardEvent.location;
+
 //hide event info when page opens
 
 
@@ -22,7 +24,27 @@ if (initialDisplay.style.display == "none");
     eventWhich.style.display = "block";
 })
 
+//event parameter is equal to whatever key is pressed on keydown
+//event stores the information relating to the key thats pressed
+window.addEventListener("keydown", (event) => {
+    document.querySelector("#keycode").innerHTML = event.keyCode;
+});
 
+window.addEventListener("keydown", (event) => {
+    document.querySelector("#eventkey").innerHTML = event.key;
+});
+
+window.addEventListener("keydown", (event) => {
+    document.querySelector("#eventcode").innerHTML = event.code;
+});
+
+window.addEventListener("keydown", (event) => {
+    document.querySelector("#eventwhich").innerHTML = event.keyCode;
+});
+
+// function input(event) {
+//     console.log("input.value" + KeyboardEevent.location);
+// }
 //initialDisplay.addEventListener("keypress", event => {
 //    event.target.textContent = event.target.textContent.hide();
 //    console.log(event);
