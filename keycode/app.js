2715ff3c26568bb6b57f120e1e67cd2dab1be32c
Joe Foster <jfoster29492@gmail.com>
2021-02-03 10:58:56 +0000

    Changes to index.html/app.js

---
+++ keycode/app.js
@@ -0,0 +1,34 @@
+const initialDisplay = document.querySelector("#initial-display");
+
+initialDisplay.addEventListener("keypress", event => {
+    event.target.textContent = event.target.textContent.hide();
+    console.log(event);
+})
+
+
+initialDisplay.addEventListener("keypress", event => {
+    console.log(event);
+})
+
+initialDisplay.addEventListener("keypress", () => {
+    if (initialDisplay.style.display === "none") {
+        initialDisplay.textContent = "show";
+        list.style.display = "block";
+    }
+    else {
+        list.style.display = "none";
+        initialDisplay.textContent = "hide";
+    }
+
+});
+
+window.addEventListener("keydown", function(event) {
+    const p = document.createElement("p");
+    p.textContent = `KeyboardEvent: key='${event.key}' | code='${event.code}'`;
+    document.querySelector("#initial-display").appendChild(p);
+  }, true);
+
+
+//function keyEvent(event) {
+//    console.log("KeyboardEvent" + event.location);
+//}
