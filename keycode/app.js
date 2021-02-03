0b33d3b5485de1d43032ddfc7fe8a578c4b3d311
Joe Foster <jfoster29492@gmail.com>
2021-02-03 11:38:31 +0000

    hide initial text added to js file

--- keycode/app.js
+++ keycode/app.js
@@ -1,32 +1,27 @@
 const initialDisplay = document.querySelector("#initial-display");
 
-initialDisplay.addEventListener("keypress", event => {
-    event.target.textContent = event.target.textContent.hide();
-    console.log(event);
-})
+//initialDisplay.addEventListener("keypress", event => {
+//    event.target.textContent = event.target.textContent.hide();
+//    console.log(event);
+//})
 
 
-initialDisplay.addEventListener("keypress", event => {
-    console.log(event);
-})
+//initialDisplay.addEventListener("keypress", event => {
+//    console.log(event);
+//})
 
-initialDisplay.addEventListener("keypress", () => {
-    if (initialDisplay.style.display === "none") {
-        initialDisplay.textContent = "show";
-        list.style.display = "block";
-    }
-    else {
-        list.style.display = "none";
-        initialDisplay.textContent = "hide";
-    }
-
+window.addEventListener("keydown", (event) => {
+        initialDisplay.style.display = "none"; 
+ //       initialDisplay.textContent = "show";
+ //       list.style.display = "block";
+    
 });
 
-window.addEventListener("keydown", function(event) {
-    const p = document.createElement("p");
-    p.textContent = `KeyboardEvent: key='${event.key}' | code='${event.code}'`;
-    document.querySelector("#initial-display").appendChild(p);
-  }, true);
+// window.addEventListener("keydown", function(event) {
+//     const p = document.createElement("p");
+//     p.textContent = `KeyboardEvent: key='${event.key}' | code='${event.code}'`;
+//     document.querySelector("#initial-display").appendChild(p);
+//   }, true);
 
 
 //function keyEvent(event) {
