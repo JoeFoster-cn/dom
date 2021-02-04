fa270c04f33adc8ab3627e717b4e8b84dba78af5
Joe Foster <jfoster29492@gmail.com>
2021-02-04 15:59:53 +0000

    activity complete
    added dice imgs, amended html, finalised js code

--- dicegame/script.js
+++ dicegame/script.js
@@ -1,37 +1,42 @@
+//variables
 const roll = document.querySelector("#roll");
 const score = document.querySelector("#score");
 const restart = document.querySelector("#restart");
 const result = document.querySelector("#player");
-const die = document.querySelector(".die");
 const dice = document.querySelector(".dice");
 
-
+//default score
 let rollScore = 0;
 
-
+//roll click event, diceroll generates number, rollscore 0 becomes 0 + the diceroll
 roll.addEventListener("click", (event) => {
     const diceRoll = Math.floor(Math.random()*6) + 1;
     rollScore = rollScore + diceRoll
- //   document.querySelector(".dice") = event.diceRoll;
+//if the rollscore is greater than or equal to 20 you win
     if (rollScore >= 20) {
         result.innerHTML = "winner";
-        score.innerHTML = rollScore;
-        restart.style.display = "block";
+        score.innerHTML = rollScore; //this is the score
+        restart.style.display = "block"; //start again button
     }
- 
+ //if diceroll hits 1 while score is less than 20, you lose
     else if (diceRoll == 1 && rollScore < 20) {
-        result.innerHTML = "You lost!";
-        score.innerHTML = rollScore;
-        restart.style.display = "block";
+        result.innerHTML = "You lost!"; //you lose
+        score.innerHTML = rollScore; //shows score
+        restart.style.display = "block"; //start again button
+        
     }
+//else show player 1, their score = to rollscore,  
     else {
         result.innerHTML = "Player 1";
         score.innerHTML = rollScore;
-        let diceNum = document.querySelector("#face-") + diceRoll;
-        document.querySelector(diceNum).style.display = "block";
+
+
     }
-   
+    let diceNum = document.getElementById("diceImg").src = "./img/dice" + diceRoll + ".png";
+    diceNum = document.querySelector("#diceImg");
+    diceNum.style.display = "block"; 
 })
+
 
 restart.addEventListener("click", (event) => {
     location.reload();
