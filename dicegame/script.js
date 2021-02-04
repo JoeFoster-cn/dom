74aece65e9c2f1a3779823d3e5c73e7e7b716787
Joe Foster <jfoster29492@gmail.com>
2021-02-04 13:12:55 +0000

    js if statement for dice rolls, winner/loser
    also amended dice on html and css

--- dicegame/script.js
+++ dicegame/script.js
@@ -1,5 +1,46 @@
+const roll = document.querySelector("#roll");
+const score = document.querySelector("#score");
+const restart = document.querySelector("#restart");
+const result = document.querySelector("#player");
+const die = document.querySelector(".die");
+const dice = document.querySelector(".dice");
+
+
+let rollScore = 0;
+
 
 roll.addEventListener("click", (event) => {
     const diceRoll = Math.floor(Math.random()*6) + 1;
-    let score = 0;
+    rollScore = rollScore + diceRoll
+ //   document.querySelector(".dice") = event.diceRoll;
+    if (rollScore >= 20) {
+        result.innerHTML = "winner";
+        score.innerHTML = rollScore;
+        restart.style.display = "block";
+    }
+ 
+    else if (diceRoll == 1 && rollScore < 20) {
+        result.innerHTML = "You lost!";
+        score.innerHTML = rollScore;
+        restart.style.display = "block";
+    }
+    else {
+        result.innerHTML = "Player 1";
+        score.innerHTML = rollScore;
+        let diceNum = document.querySelector("#face-") + diceRoll;
+        document.querySelector(diceNum).style.display = "block";
+    }
+   
 })
+
+restart.addEventListener("click", (event) => {
+    location.reload();
+});
+
+/*trying to use roll button to reset page after losing
+roll.addEventListener("click", (event) => {
+    if (result == "You lost!")
+    location.reload();
+});*/
+
+ 
