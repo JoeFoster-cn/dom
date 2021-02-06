9f42e35c842ed67d088994ad292d4946bd2596d2
Joe Foster <jfoster29492@gmail.com>
2021-02-06 00:12:39 +0000

    completed activity
    tidied all code, added final js code and comments

--- dicegame2/script.js
+++ dicegame2/script.js
@@ -1,4 +1,4 @@
-//variables
+//variables list
 const restart = document.querySelector("#restart");
 const roll = document.querySelector("#roll");
 const hold = document.querySelector("#hold");
@@ -8,133 +8,82 @@ const score1 = document.querySelector("#score1");
 const score2 = document.querySelector("#score2");
 let cScore1 = document.querySelector("#currentscore1");
 let cScore2 = document.querySelector("#currentscore2");
-let activeplayer = 1;
-
-
-//new game to begin, show roll dice and hold buttons
-restart.addEventListener("click", (event) => {
-    roll.style.display = "block";
-    hold.style.display = "block";
-    if (won == true) {
-        location.reload();}
-});
-
+const box1 = document.querySelector("#box1");
+const box2 = document.querySelector("#box2");
 let rollScore1 = 0;
 let rollScore2 = 0;
 let won = false;
+let activeplayer = 1;
 
+//begin by clicking new game, show roll dice and hold buttons
+restart.addEventListener("click", (event) => {
+    roll.style.display = "block"; //display roll button
+    hold.style.display = "block"; //display hold button
+    if (won == true) { //if someone has won and new game is clicked, the page refreshes
+        location.reload();}
+});
 
-//P1 click roll, dice shows with the number rolled 
+//roll button
 roll.addEventListener("click", (event) => {
-    let diceRoll = Math.floor(Math.random()*6) + 1;
+    let diceRoll = Math.floor(Math.random()*6) + 1; //diceroll number generates
     let diceNum = document.getElementById("diceImg").src = "./img/dice" + diceRoll + ".png";
-        diceNum = document.querySelector("#diceImg");
+        diceNum = document.querySelector("#diceImg"); //dice image shows based on diceroll
         diceNum.style.display = "block"; 
 
-    if (activeplayer == 1) { 
-        if (diceRoll == 1) {
-            activeplayer = 2;
-            cScore1.textContent = 0;
+    if (activeplayer == 1) {  //active player select
+        if (diceRoll == 1) {  //has a 1 been rolled?
+            activeplayer = 2; //if 1 is rolled, swap to player2
+            cScore1.textContent = 0; //clear player1 current score
             reset()
             return
         }
-        rollScore1 = rollScore1 += diceRoll;
-        cScore1.textContent = rollScore1;
-        if (parseInt(score1.textContent) >= 20) {
-            won = true;
-            player1.textContent = "Winner";
-            cScore1.textContent = rollScore1;
-        }
+        rollScore1 = rollScore1 += diceRoll; //player1 rolls dice
+        cScore1.textContent = rollScore1; //player1 current score updates
     }
-    else {
+    else { //if not player1, then it's player2
         if (diceRoll === 1) {
-            activeplayer = 1;
-            cScore2.textContent = 0;
+            activeplayer = 1; //if 1 is rolled, swap to player1
+            cScore2.textContent = 0; //clear player2 current score
             reset()
-            return
-            
+            return   
         }
-        rollScore2 = rollScore2 += diceRoll;
-        cScore2.textContent = rollScore2;
-        console.log(score1.textContent);
-        if (parseInt(score1.textContent) >= 20) {
-            console.log("Hello");
-            won = true;
-            player2.textContent = "Winner"
-            cScore2.textContent = rollScore2;
-        }
+        rollScore2 = rollScore2 += diceRoll; //player2 rolls dice
+        cScore2.textContent = rollScore2; //player2 current score
     }
 });
 
 hold.addEventListener("click", (event) => {
-    if (activeplayer == 1) {
-        score1.textContent = parseInt(score1.textContent) + rollScore1; 
-        activeplayer = 2;
+    if (activeplayer == 1) { //if it's player1 
+        score1.textContent = parseInt(score1.textContent) + rollScore1; //adds current score to held score
+        if (parseInt(score1.textContent) >= 20) { //if held score is greater than or equal to 20, player1 wins
+            won = true; //set won to true
+            player1.textContent = "Winner"; //set player1 to winner
     }
-       
-    else if (activeplayer = 2) {
-        score2.textContent = parseInt(score2.textContent) + rollScore2;
-        activeplayer = 1
+    activeplayer = 2; //swap to player 2
+    box1.classList.remove("box1"); //swap the colour of the boxes based on the winner
+    box1.classList.add("box2");
+    box2.classList.add("box1");
+    box2.classList.remove("box2");
+}    
+    else if (activeplayer = 2) { //if it's player2
+        score2.textContent = parseInt(score2.textContent) + rollScore2; //adds current score to held score
+        if (parseInt(score2.textContent) >= 20) { //if held score is greater than or equal to 20, player2 wins
+            won = true; //set won to true
+            player2.textContent = "Winner"; //set player2 to winner
     }
+    activeplayer = 1;
+    box1.classList.remove("box1"); //swap the colour of the boxes based on the winner
+    box1.classList.add("box2");
+    box2.classList.add("box1");
+    box2.classList.remove("box2");
+} 
     reset()
 
 });
 
-
-function reset() {
+function reset() { //reset current score and dice rolls for both players
     cScore1.textContent = 0;
     cScore2.textContent = 0;
     rollScore1 = 0;
     rollScore2 = 0
-}
-
-
-
-
-// restart.addEventListener("click", (event) => {
-//     if (won == true) {
-//         location.reload();}
-// });
-
-
-// hold.addEventListener("click", (event => {
-//     score1.innerHTML = cScore1;
-
-// }))
-
-// //default score
-// let rollScore = 0;
-
-// //roll click event, diceroll generates number, rollscore 0 becomes 0 + the diceroll
-// roll.addEventListener("click", (event) => {
-//     const diceRoll = Math.floor(Math.random()*6) + 1;
-//     rollScore = rollScore += diceRoll
-// //if the rollscore is greater than or equal to 20 you win
-//     if (rollScore >= 20) {
-//         result.innerHTML = "winner";
-//         score.innerHTML = rollScore; //this is the score
-//         restart.style.display = "block"; //start again button
-//     }
-//  //if diceroll hits 1 while score is less than 20, you lose
-//     else if (diceRoll == 1 && rollScore < 20) {
-//         result.innerHTML = "You lost!"; //you lose
-//         score.innerHTML = rollScore; //shows score
-//         restart.style.display = "block"; //start again button
-//     }
-// //else show player 1, their score = to rollscore,  
-//     else {
-//         result.innerHTML = "Player 1";
-//         score.innerHTML = rollScore;
-// //img shortcut + variable + file name
-//     }
-//     let diceNum = document.getElementById("diceImg").src = "./img/dice" + diceRoll + ".png";
-//     diceNum = document.querySelector("#diceImg");
-//     diceNum.style.display = "block"; 
-// })
-// restart.addEventListener("click", (event) => {
-//     location.reload();
-// });
-
-
-
- 
+} 
